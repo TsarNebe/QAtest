7dfe1bde0e90920538add71662b6583345639389
egor <egor.tupchienko@gmail.com>
2022-11-07 12:47:03 +0200

    changed type of variable age to int

---
+++ FirstDir/second2.py
@@ -0,0 +1,13 @@
+from Another import first
+
+print (type(first.name))
+print (type(first.surname))
+print (id(first.name))
+print (id(first.surname))
+
+age = 33
+print(type(age))
+print(id(age))
+age = 45
+print(type(age))
+print(id(age))
